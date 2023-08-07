3c349eb6cf9b1e289930f7e33f394b7259bf3fcd
Sammy Alexander Ayra <jayi.darksam@gmail.com>
2023-08-07 14:53:20 -0500

    se avanza

---
+++ little_face_xamarin/little_face/little_face/Data/Enumerations/RoleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace little_face.Data.Enumerations
+{
+    public enum RoleType
+    {
+        SuperAdmin,
+        Staff,
+        Guest
+    }
+}
---
+++ little_face_xamarin/little_face/little_face/Data/Models/UserRole.cs
@@ -0,0 +1,21 @@
+﻿using little_face.Data.Enumerations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace little_face.Data.Models
+{
+    public class UserRole
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long RoleId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public RoleType Type { get; set; }
+
+    }
+}
