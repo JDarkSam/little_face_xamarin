c6b06b166c8df68d6db620dcf789cc9f98c98578
Sammy Alexander Ayra <jayi.darksam@gmail.com>
2023-08-23 23:56:56 -0500

    se afina Dto de infantes

--- little_face_xamarin/little_face/little_face/Data/Models/Dto/ChildDto.cs
+++ little_face_xamarin/little_face/little_face/Data/Models/Dto/ChildDto.cs
@@ -2,18 +2,12 @@
 namespace little_face.Data.Models.Dto
 {
     public class ChildDto
-    {   
+    {
+        public long Id { get; set; }
         public string Names { get; set; }
-
-       
         public string Surnames { get; set; }
-
-       
         public int Age { get; set; }
-
-       
         public string Alias { get; set; }
-
         public long UserId { get; set; }
 
     }
